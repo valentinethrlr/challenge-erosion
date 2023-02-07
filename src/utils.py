cb48007cc977a69f4c1f160432fcfe0f92015a62
valentinethrlr <v.thuerler@bluewin.ch>
2023-02-07 15:38:09 +0000

    modifs

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ def img2ascii(img_data: list[list[int]], black: str = '#', white: str = '.') -> 
     '''
     Change the image in list in an image in string
     '''
-     return ''.join([''.join([black if char == 1 else white for char in line] + ['\n']) for line in img_data])
+    return ''.join([''.join([black if char == 1 else white for char in line] + ['\n']) for line in img_data])[:-1]
 
 
 

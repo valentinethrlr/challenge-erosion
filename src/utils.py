6cadbc7f4dc4052ebe06a74bf5956f982260409b
valentinethrlr <v.thuerler@bluewin.ch>
2023-02-09 19:39:48 +0000

    modifs

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,30 @@ def img2ascii(img_data: list[list[int]], black: str = '#', white: str = '.') -> 
     return ''.join([''.join([black if char == 1 else white for char in line] + ['\n']) for line in img_data])[:-1]
 
 
+def load_pbm(filename: str) -> list[list[int]]:
+    '''
+    load the data in the file and return it in a list form
+    '''
+    with open(filename, 'r') as f:
+        data = [line.replace("\n", "").split(" ") for line in f if line[0] != '#']
+    
+    size = data[1]
+    row = int(size[0])
+    line = int(size[1])
+    image = list(''.join([item for sublist in data[2:] for item in sublist]))
+    final_list = split_list(image, line, row)
+    return final_list
+
+def split_list(data: list, line: int, row: int) -> list[list[int]]:
+    i: int = 0
+    current_list = []
+    final_list = []
+    for _ in range(line):
+        for _ in range(row):
+            current_list += [int(data[i])]
+            i += 1
+        final_list += [current_list]
+        current_list = []
+    return final_list   
+   
+print(load_pbm('circle-nb-3.pbm'))
